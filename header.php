9526f2b85978d3f66ae371b8a45d158bcea67011
Zeeshan M. <zeeshanmujtaba17@gmail.com>
2025-02-16 23:19:42 -0500

    Implemented  method to enable  theme support.

--- header.php
+++ header.php
@@ -6,15 +6,16 @@
  */
 ?>
 
-<!DOCTYPE html>
-<html lang="<?php language_attributes(); ?>">
-  <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <?php wp_head(); ?>
-  </head>
-  <body <?php body_class(); ?>>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
   
   <?php 
   //wp_body_open() is a new function in WordPress 5.2 that adds backward compatibility for themes that do not support the function.
--- inc/classes/class-custom-theme.php
+++ inc/classes/class-custom-theme.php
@@ -25,6 +25,10 @@
         /*
          * Actions
          */
-        
+        add_action( 'after_setup_theme', [ $this, 'setup_theme' ] );  
+    }
+    public function setup_theme() {
+        // Enable Website Title be dynamically added through WordPress (Apprearance > Customize > Site Identity)
+        add_theme_support('title-tag');
     }
 }
