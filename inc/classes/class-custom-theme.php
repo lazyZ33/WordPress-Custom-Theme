2e3d1e663c9024d437df469e4d406436997ae759
Zeeshan M. <zeeshanmujtaba17@gmail.com>
2025-02-17 00:53:55 -0500

    Added Theme Functionalities like dynamic title & logo, post-thumbnails, customize-selective-refresh, Automatic Feed Links, HTML5 Theme Support, Editor Style, content width, gutenberg editor style

--- inc/classes/class-custom-theme.php
+++ inc/classes/class-custom-theme.php
@@ -28,7 +28,52 @@
         add_action( 'after_setup_theme', [ $this, 'setup_theme' ] );  
     }
     public function setup_theme() {
-        // Enable Website Title be dynamically added through WordPress (Apprearance > Customize > Site Identity)
+        // Enable Website Title to be dynamically Added/Updated through WordPress. (Appearance > Customize > Site Identity)
         add_theme_support('title-tag');
+
+        // Enable Website Logo to be dynamically Added/Updated through WordPress. (Appearance > Customize > Site Identity > Logo)
+        add_theme_support('custom-logo', [
+            'header-text'          => array('site-title', 'site-description'),
+            'height'               => 100,
+            'width'                => 400,
+            'flex-height'          => true,
+            'flex-width'           => true,
+            'unlink-homepage-logo' => true,
+        ]);
+
+        // Add Featured Image Option on Posts and Pages.
+        add_theme_support('post-thumbnails');
+
+        // Enable Selective Refresh for Widgets in the Customizer. (Improves live preview performance)
+        add_theme_support('customize_selective_refresh_widgets');
+
+        // Enable Automatic Feed Links for RSS Feeds. (Adds RSS feed links to the <head> section of your site)
+        add_theme_support('automatic-feed-links');
+
+        // Enable HTML5 Markup for Core Features. (Replaces older XHTML markup with modern HTML5)
+        add_theme_support('html5', [
+            'search-form', 
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+            'style',
+            'script',
+        ]);
+
+        // Add Editor Styles for the WordPress Visual Editor. (Ensures consistency between the editor and frontend styles)
+        add_editor_style();
+
+        // Enable Default Styles for WordPress Core Blocks.
+        add_theme_support('wp-block-styles');
+
+        //Gives Align Wide and Full Width for Gutenberg Blocks.
+        add_theme_support('align-wide');
+
+        //Set Global Content max-width (if not defined)
+        global $content_width;
+        if(!isset ($content_width)){
+            $content_width = 1240;
+        }
     }
 }
