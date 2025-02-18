cfd8d6bee4793e35935d2eeb0a316dbe8480aafd
Zeeshan M. <zeeshanmujtaba17@gmail.com>
2025-02-17 21:15:06 -0500

    Updated class-menu.php and nav.php to get dynamic navbar but dropdown is not working.

--- inc/classes/class-menus.php
+++ inc/classes/class-menus.php
@@ -1,33 +1,80 @@
 <?php
 /**
-* Register Menus
-*
-* @package Custom Theme
-*/
+ * Register Menus
+ *
+ * @package Custom Theme
+ */
 
 namespace CUSTOM_THEME\Inc;
 
 use CUSTOM_THEME\Inc\Traits\Singleton;
 
 class Menus {
-	use Singleton;
-    
-    protected function __construct() {
-        // Load classes
-        $this->set_hooks();
-    }
-    protected function set_hooks() {
-        /*
-         * Actions
-         */
-        add_action('init', [$this, 'register_menus']);
-    }
 
-    public function register_menus(){
-        register_nav_menus([
-            'custom-theme-header-menu' => esc_html__('Header Menu', 'custom-theme'),
-            'custom-theme-footer-menu' => esc_html__('Footer Menu', 'custom-theme'),
-        ]);
-    }
+	use Singleton;
+
+	protected function __construct() {
+
+		// load class.
+		$this->setup_hooks();
+	}
+
+	protected function setup_hooks() {
+
+		/**
+		 * Actions.
+		 */
+		add_action( 'init', [ $this, 'register_menus' ] );
+	}
+
+	public function register_menus() {
+		register_nav_menus([
+			'custom-theme-header-menu' => esc_html__( 'Header Menu', 'custom-theme' ),
+			'custom-theme-footer-menu' => esc_html__( 'Footer Menu', 'custom-theme' ),
+		]);
+	}
+
+	/**
+	 * Get the menu id by menu location.
+	 *
+	 * @param string $location
+	 *
+	 * @return integer
+	 */
+	public function get_menu_id( $location ) {
+
+		// Get all locations
+		$locations = get_nav_menu_locations();
+
+		// Get object id by location.
+		$menu_id = ! empty($locations[$location]) ? $locations[$location] : '';
+
+		return ! empty( $menu_id ) ? $menu_id : '';
+
+	}
+
+	/**
+	 * Get all child menus that has given parent menu id.
+	 *
+	 * @param array   $menu_array Menu array.
+	 * @param integer $parent_id Parent menu id.
+	 *
+	 * @return array Child menu array.
+	 */
+	public function get_child_menu_items( $menu_array, $parent_id ) {
+
+		$child_menus = [];
+
+		if ( ! empty( $menu_array ) && is_array( $menu_array ) ) {
+
+			foreach ( $menu_array as $menu ) {
+				if ( intval( $menu->menu_item_parent ) === $parent_id ) {
+					array_push( $child_menus, $menu );
+				}
+			}
+		}
+
+		return $child_menus;
+	}
 
 }
--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -1,53 +1,90 @@
 <?php
-/*
-* Navigation template
-*
-* @package Custom Theme
-*/ 
+/**
+ * Header Navigation template.
+ *
+ * @package Custom Theme
+ */
+
+$menu_class     = \Custom_Theme\Inc\Menus::get_instance();
+$header_menu_id = $menu_class->get_menu_id( 'custom-theme-header-menu' );
+$header_menus   = wp_get_nav_menu_items( $header_menu_id );
 ?>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <?php
-      the_custom_logo();
-    ?>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light py-4">
+	<div class="container">
+		<?php
+		if ( function_exists( 'the_custom_logo' ) ) {
+			the_custom_logo();
+		}
+		?>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+		        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<?php
+			if ( ! empty( $header_menus ) && is_array( $header_menus ) ) {
+				?>
+				<ul class="navbar-nav mr-auto">
+					<?php
+					foreach ( $header_menus as $menu_item ) {
+						if ( ! $menu_item->menu_item_parent ) {
+
+							$child_menu_items   = $menu_class->get_child_menu_items( $header_menus, $menu_item->ID );
+							$has_children       = ! empty( $child_menu_items ) && is_array( $child_menu_items );
+							$has_sub_menu_class = ! empty( $has_children ) ? 'has-submenu' : '';
+							$link_target        = ! empty( $menu_item->target ) && '_blank' === $menu_item->target ? '_blank' : '_self';
+
+							// Note_: Similar to $menu_item->target, there are other keys available in the $menu_item, such as classes. You can more key values if you need.
+
+							if ( ! $has_children ) {
+								?>
+								<li class="nav-item">
+									<a class="nav-link" href="<?php echo esc_url( $menu_item->url ); ?>"
+									   target="<?php echo esc_attr( $link_target ); ?>"
+									   title="<?php echo esc_attr( $menu_item->title ); ?>">
+										<?php echo esc_html( $menu_item->title ); ?>
+									</a>
+								</li>
+								<?php
+							} else {
+								?>
+								<li class="nav-item dropdown">
+									<a class="nav-link dropdown-toggle" href="<?php echo esc_url( $menu_item->url ); ?>"
+									   id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+									   aria-expanded="false" target="<?php echo esc_attr( $link_target ); ?>"
+									   title="<?php echo esc_attr( $menu_item->title ); ?>">>
+										<?php echo esc_html( $menu_item->title ); ?>
+									</a>
+									<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+										<?php
+										foreach ( $child_menu_items as $child_menu_item ) {
+											$link_target = ! empty( $child_menu_item->target ) && '_blank' === $child_menu_item->target ? '_blank' : '_self';
+											?>
+											<a class="dropdown-item"
+											   href="<?php echo esc_url( $child_menu_item->url ); ?>"
+											   target="<?php echo esc_attr( $link_target ); ?>"
+											   title="<?php echo esc_attr( $child_menu_item->title ); ?>">>
+												<?php echo esc_html( $child_menu_item->title ); ?>
+											</a>
+											<?php
+										}
+										?>
+									</div>
+								</li>
+								<?php
+							}
+							?>
+							<?php
+						}
+					}
+					?>
+				</ul>
+				<?php
+			}
+			?>
+			<?php get_search_form(); ?>
+		</div>
+	</div>
 </nav>
-
-<?php
-  wp_nav_menu([
-      'theme_location' => 'custom-theme-header-menu',
-      'container-class' => 'my_extra_menu_class'
-  ]);
-?>
