fd40f553c015ec8bc723a51f16438a4385e519b3
Zeeshan M. <zeeshanmujtaba17@gmail.com>
2025-02-26 19:59:27 -0800

    Fixed the dropdown issue by updating the dropdown li and including popper.js

--- inc/classes/class-assets.php
+++ inc/classes/class-assets.php
@@ -37,10 +37,12 @@ class Assets {
 
     public function register_scripts() {
         // Register Scripts.
+        wp_register_script('popper-js', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js', [], null, true);
         wp_register_script('main-js', CUSTOM_THEME_DIR_URI . '/assets/main.js', [], filemtime(CUSTOM_THEME_DIR_PATH . '/assets/main.js'), true);
         wp_register_script('bootstrap-js', CUSTOM_THEME_DIR_URI . '/assets/src/library/js/bootstrap.min.js', ['jquery'], false, true);
 
         // Enqueue Scripts.
+        wp_enqueue_script('popper-js');
         wp_enqueue_script('main-js');
         wp_enqueue_script('bootstrap-js');
     }
